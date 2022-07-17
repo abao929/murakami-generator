9bc4e31deff29a321af9c8fcccaa27a9f19e089c
abao929 <44300045+abao929@users.noreply.github.com>
2022-07-16 20:26:05 -0400

    wait

---
+++ src/types/global.d.ts
@@ -0,0 +1,28 @@
+export {}
+declare global {
+  interface Position {
+    x: number
+    y: number
+  }
+
+  interface FlowerColors {
+    petal0: string
+    petal1: string
+    petal2: string
+    petal3: string
+    petal4: string
+    petal5: string
+    petal6: string
+    petal7: string
+    petal8: string
+    petal9: string
+    petal10: string
+    petal11: string
+    oddPetals: string
+    evenPetals: string
+    leftEye: string
+    rightEye: string
+    face: string
+    mouth: string
+  }
+}
